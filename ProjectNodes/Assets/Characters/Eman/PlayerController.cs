405e032d0a71e1f56576c04e152b7651fbd805a4
nicksiuxs <nicksiuxs@gmail.com>
2020-03-28 10:54:28 -0500

    Se creó lac arptea Characters, la escena, material fisico y el PlayerController

---
+++ ProjectNodes/Assets/Characters/Eman/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0, 0.3f)] private float m_MovementSmoothing = 0.02f;
+    private Rigidbody2D rigidBody;
+    private Vector3 m_velocity = Vector3.zero;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float inputMovement = Input.GetAxisRaw("Horizontal");
+        horizontalMove = inputMovement * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        move(horizontalMove * Time.fixedDeltaTime);
+    }
+    public void move(float move)
+    {
+        Vector3 targetVelocity = new Vector2(move * 10f, rigidBody.velocity.y);
+        rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref m_velocity, m_MovementSmoothing);
+    }
+}
