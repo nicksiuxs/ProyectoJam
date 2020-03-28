d8ecf92827f2a54d8492a561a4ea8815525b18e0
nicksiuxs <nicksiuxs@gmail.com>
2020-03-28 12:46:31 -0500

    Se creó el script GroundedController

---
+++ ProjectNodes/Assets/Characters/Eman/GroundedController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedController : MonoBehaviour
+{
+    public int groundMask;
+    public PlayerController playerController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == groundMask)
+        {
+            playerController.setIsGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == groundMask)
+        {
+            playerController.setIsGrounded(false);
+        }
+    }
+}
