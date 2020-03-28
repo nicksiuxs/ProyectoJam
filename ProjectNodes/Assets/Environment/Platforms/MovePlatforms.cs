fc488c21fbbd224b1172e92559cd8684f7e6f60f
juanda3098 <juandapore3098@gmail.com>
2020-03-28 10:26:10 -0500

    Se creó la carpeta Environment

---
+++ ProjectNodes/Assets/Environment/Platforms/MovePlatforms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatforms : MonoBehaviour
+{
+
+    private Vector3 positionA;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
