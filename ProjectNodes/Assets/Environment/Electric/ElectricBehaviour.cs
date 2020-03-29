30e9fca8be002571a9434602d4a115179dfee476
acarcila <acarcila98@gmail.com>
2020-03-29 12:58:13 -0500

    Se creó el ElectricController y se programó la mecánica de electricidad

--- ProjectNodes/Assets/Environment/Electric/ElectricBehaviour.cs
+++ ProjectNodes/Assets/Environment/Electric/ElectricBehaviour.cs
@@ -6,47 +6,34 @@ public class ElectricBehaviour : MonoBehaviour
 {
     public bool isOn;
     public bool alwaysOn;
-    // public float electricFieldRange;
-    // public LayerMask electricLayer;
-
-    // private List<ElectricBehaviour> sourceElements = new List<ElectricBehaviour>();
+    public float electricFieldRange;
+    public LayerMask electricLayer;
+    public CircleCollider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        setUp();
+    }
+
+    void OnValidate()
+    {
+        setUp();
+    }
+
+    public void setUp()
+    {
+        collider.radius = electricFieldRange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // checkElectricRange();
-        // Debug.Log(electricLayer.value);
+
     }
 
-    // void OnDrawGizmos()
-    // {
-    //     Gizmos.DrawWireSphere(gameObject.transform.position, electricFieldRange);        
-    // }
-
-    // private void checkElectricRange()
-    // {
-    //     Collider2D[] electricElements = Physics2D.OverlapCircleAll(gameObject.transform.position, electricFieldRange, electricLayer);
-    //     List<ElectricBehaviour> sourceElements = new List<ElectricBehaviour>();
-
-    //     foreach(Collider2D element in electricElements)
-    //     {
-    //         ElectricBehaviour electricElement = element.GetComponent<ElectricBehaviour>();
-    //         if(electricElement.isOn)
-    //         {
-    //             sourceElements.Add(electricElement);
-    //             isOn = true;
-    //         }            
-    //     }
-
-    //     if(sourceElements.Count == 0)
-    //     {
-    //         isOn = false;
-    //     }
-    // }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(gameObject.transform.position, electricFieldRange);
+    }
 }
---
+++ ProjectNodes/Assets/Environment/Electric/ElectricityController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityController : MonoBehaviour
+{
+
+    public LayerMask electricLayer;
+
+    public List<ElectricBehaviour> sourceList;
+    public List<ElectricBehaviour> nodesList;
+    public List<ElectricBehaviour> checkingList;
+    public List<ElectricBehaviour> toCheckList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setUp();
+    }
+
+    void OnValidate()
+    {
+        // setUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkList();
+
+    }
+
+    private void setUp()
+    {
+        var electricElements = FindObjectsOfType<ElectricBehaviour>();
+        foreach (ElectricBehaviour element in electricElements)
+        {
+            if (element.alwaysOn)
+            {
+                sourceList.Add(element);
+            }
+            else
+            {
+                nodesList.Add(element);
+            }
+        }
+
+        toCheckList = new List<ElectricBehaviour>(nodesList);
+
+        checkingList = new List<ElectricBehaviour>(sourceList);
+    }
+
+    private void checkList()
+    {
+        List<ElectricBehaviour> checkingListTemporal = new List<ElectricBehaviour>();
+        foreach (ElectricBehaviour element in checkingList)
+        {
+            Debug.Log("checking: " + element.name);
+            Collider2D[] collidedElements = Physics2D.OverlapCircleAll(element.gameObject.transform.position, element.electricFieldRange, electricLayer);
+
+            foreach (Collider2D collider in collidedElements)
+            {
+                Debug.Log(collider.name);
+                ElectricBehaviour electricBehaviour = collider.gameObject.GetComponent<ElectricBehaviour>();
+                if (toCheckList.Exists(e => e == electricBehaviour))
+                {
+                    checkingListTemporal.Add(electricBehaviour);
+                    toCheckList.Remove(electricBehaviour);
+                }
+                electricBehaviour.isOn = true;
+            }
+        }
+
+        checkingList = checkingListTemporal;
+        if (checkingList.Count > 0)
+        {
+            checkList();
+        }
+        else
+        {
+            foreach (ElectricBehaviour electricBehaviour in toCheckList)
+            {
+                electricBehaviour.isOn = false;
+            }
+
+            toCheckList = new List<ElectricBehaviour>(nodesList);
+
+            checkingList = new List<ElectricBehaviour>(sourceList);
+        }
+    }
+}
