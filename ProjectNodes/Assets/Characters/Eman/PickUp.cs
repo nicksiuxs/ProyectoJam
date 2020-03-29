36e76c9b88ab3fd15b902220bb00ac3aa40d6c17
nicksiuxs <nicksiuxs@gmail.com>
2020-03-28 19:32:34 -0500

    Se creó el script que permite recoger y dejar objetos

---
+++ ProjectNodes/Assets/Characters/Eman/PickUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Rigidbody2D itemRigidBody;
+    public Rigidbody2D playerRigidBody;
+
+    public float throwForce = 0;
+    bool hasPlayer = false;
+    bool beingCarried = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        itemRigidBody = other.GetComponent<Rigidbody2D>();
+        hasPlayer = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        hasPlayer = false;
+    }
+
+    void Update()
+    {
+        if (beingCarried)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                itemRigidBody.isKinematic = false;
+                itemRigidBody.transform.parent = null;
+                beingCarried = false;
+                transform.position= playerRigidBody.transform.position;
+                //playerRigidBody.AddForce(playerRigidBody.transform.forward * throwForce);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0) && hasPlayer)
+            {
+                itemRigidBody.isKinematic = true;
+                itemRigidBody.transform.parent = playerRigidBody.transform;
+                beingCarried = true;
+            }
+        }
+    }
+}
