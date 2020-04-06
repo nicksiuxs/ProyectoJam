99f7958de93067937c58a06da0e8d383bd7b3621
acarcila <acarcila98@gmail.com>
2020-04-05 22:35:21 -0500

    Se creó el nodo Actuador y se creó la garra mecánica

---
+++ ProjectNodes/Assets/Environment/Electric/Nodes/ActuatorNode/ActuatorNodeElectricBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActuatorNodeElectricBehaviour : ElectricBehaviour
+{
+
+    public override void toStart()
+    {
+    }
+
+    public override void toUpdate()
+    {
+    }
+
+    public override void toOnDrawGizmos()
+    {
+    }
+
+    public override List<ElectricBehaviour> getCollidedElements()
+    {
+        List<ElectricBehaviour> electricBehaviourList = new List<ElectricBehaviour>();
+        List<Collider2D> colliders = base.electricFieldController.getCollidedElements();
+
+        foreach(Collider2D collider in colliders)
+        {
+            electricBehaviourList.Add(collider.GetComponentInParent<ElectricBehaviour>());
+        }
+
+        return electricBehaviourList;
+    }
+
+    public override List<ElectricBehaviour> handleActivate()
+    {
+        base.isOn = true;
+        List<ElectricBehaviour> electricElements = new List<ElectricBehaviour>();
+        electricElements.Add(this);
+
+        base.activateParticleSystem();
+
+        return electricElements;
+    }
+
+    public override void handleDeactivate()
+    {
+        base.isOn = false;
+        base.deactivateParticleSystem();
+    }
+}
